5e648d8d9d4554d98aed84b348063fcb4ada91c5
PierrotHml <pierre.hamel.eure@gmail.com>
2016-12-16 10:07:40 +0100

    MMU - fonction main termSaver3

--- main.c
+++ main.c
@@ -1,80 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 #include "functions.h"
 
 int main(int argc, char *argv[])
 {
-	Coordinates consoleSize, imgSize; //variables coordonnées contenant des tailles x, y.
+	Coordinates consoleSize;
+    Coordinates imgSize[4];
 
-	captureConsoleSize(&consoleSize, imgSize); //fonction stockant la taille de la console dans consoleSize.
+    Coordinates planePosition;
 
-	pid_t pid; //variable contenant un pid.
+    Direction i; //création de i pouvant prendre 
 
-	FILE* img = NULL; //pointeur vers un fichier.
+	char planePath[256]; //création d'une chaine de caractère
 
-	img = fopen(argv[1] , "r"); //ouverture de l'image pbm envoyée en argument.
+    int imgPixel[4][6][6]; //création de 4 tableaux bi-dimensionnels pouvant contenir des données binaires d'une image pbm.
 
- 	if (img == NULL) //erreur si échec de l'ouverture de l'image.
-    {
-        printf("ERROR: Impossible to open %s (inexisting file ?).", argv[1]);
-        exit(-1); 
-    }
+    int cnt; //cnt est un compteur.
 
-    readImgFormat(img); //lis le format du pbm
+    planePosition.x = atoi(argv[2]);
+    planePosition.y = atoi(argv[3]); //attribution des coordonnées initiales de l'avion à planePosition.
 
-	readImgSize(img, &imgSize); //lis la taille du pbm.
+	captureConsoleSize(&consoleSize); //fonction cherchant la taille de la console.
 
-    int imgPixel[imgSize.x][imgSize.y]; //créer un tableau de la taille du pbm.
-
-	readImgPixels(img, imgSize, imgPixel); //rempli le tableau avec les données binaires de l'image.
-
-    fclose(img); //ferme le fichier image.
-
-    Frame imgFrame; //variable contenant un cadre
-
-    int continuee; //variable servant )à arrêter le programme.
-
-    continuee = 1;
-
-	pid = fork(); //création d'un processus fils
-
-	if(pid == -1)
+	if(planePosition.x > consoleSize.x || planePosition.x < 0 ||  planePosition.y > consoleSize.y || planePosition.y < 0) //erreur si la position initiale de l'avion  sont hors du terminal.
 	{
-		printf("ERROR: Failure to create a new process."); //erreur en cas d'échec processus fils.
+		printf("ERROR: Plane position is out of console size.");
 		exit(-1);
 	}
-	else if(pid ==  0)
-	{
-		getch(); //attend que l'utilisateur tape sur son clavier.
 
-		kill(0, SIGTERM); //tue le processus dans la boucle infinie.
-	}
-	else
+	FILE* img = NULL; //création d'une variable pointant ensuite dans un fichier.
+
+	for(cnt = 0; cnt < 4; cnt++) //répétition de l'ouverture d'une image 4 fois, l'image changeant grâce à la variable cnt.
 	{
-		while(continuee)
+		strcpy(planePath, argv[1]);
+
+		switch(cnt) //ajout du nom de l'image à planePath.
 		{
-			switch(captureConsoleSize(&consoleSize, imgSize))
-			{
-				case -1:
-					continuee = 0; //arrête la boucle si l'image est plus petite que la console.
-					break;
-				case 1:
-					system("clear"); //nettoye le terminal visible.
-					
-					centerImg(imgSize, consoleSize, &imgFrame); //créer un cadre centrant l'image.
-					
-					printImg(consoleSize, imgSize, imgFrame, imgPixel); //affiche l'image sur la console.
-
-					break;
-			}
-
-			fflush(stdout);
-			sleep(1); //attend une seconde avant de répéter la boucle.
+			case 0:
+				strcat(planePath, "plane_up.pbm");
+				break;
+			case 1: 
+				strcat(planePath, "plane_right.pbm");
+				break;
+			case 2:
+				strcat(planePath, "plane_down.pbm");
+				break;
+			case 3:
+				strcat(planePath, "plane_left.pbm");
+				break;
 		}
+
+		img = fopen(planePath , "r"); //ouverture de l'image pbm.
+
+	 	if (img == NULL) //erreur si impossible d'ouvrir une image pbm.
+	    {
+	        printf("ERROR: unable to open %s (inexisting file ?).", planePath);
+	        exit(-1); 
+	    }
+
+
+	   	readImgFormat(img); //lis le format des pbm.
+
+		readImgSize(img, &imgSize[cnt]); //lis la taille des pbm.
+
+		readImgPixels(img, imgSize[cnt], imgPixel[cnt]); //lis les pixels de l'avion pour les écrire dans le tableau imgPixel[cnt].
 	}
+
+	Frame imgFrame; //création de imgFrame, contenant le cadre de l'avion.
+
+    i = RIGHT; //initialisation de i.
+
+    while(1)
+    {
+    	moveImg(i, consoleSize, &planePosition); //Fonction faisant bouger l'avion en fonction de la direction i.
+
+		frameImg(consoleSize, planePosition, &imgFrame); //Fonction cadrant l'avion en fonction de son emplacement.
+
+		system("clear"); //nettoyage du terminal.
+
+		printImg(consoleSize, imgFrame, imgPixel[i]); //affichage de l'image.
+
+		fflush(stdout);
+		sleep(0.5); //attente de 0.5 secondes avant de faire bouger l'avion.
+
+		changeDirection(&i); //Fonction changeant la valeur de i si l'utilisateur appuie sur Z Q S D.
+    }
 
 	return 0;
 }
