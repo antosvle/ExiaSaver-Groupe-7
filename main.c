1bb397d9280f04e981c99ff1c56fad61abac3485
PierrotHml <pierre.hamel.eure@gmail.com>
2016-12-16 10:02:18 +0100

    MMU - fonction main termSaver1

--- main.c
+++ main.c
@@ -1,45 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include "fonctions.h"
+#include <sys/wait.h>
+#include <unistd.h>
+#include "functions.h"
 
 int main(int argc, char *argv[])
 {
-    if(argc > 3) // nombre d'aguments suppérieur à 3
+	Coordinates consoleSize, imgSize; //variables coordonnées contenant des tailles x, y.
+
+	captureConsoleSize(&consoleSize, imgSize); //fonction stockant la taille de la console dans consoleSize.
+
+	pid_t pid; //variable contenant un pid.
+
+	FILE* img = NULL; //pointeur vers un fichier.
+
+	img = fopen(argv[1] , "r"); //ouverture de l'image pbm envoyée en argument.
+
+ 	if (img == NULL) //erreur si échec de l'ouverture de l'image.
     {
-        printf("ERROR: Too many arguments\n"); // affiche une erreur
-        exit(-1); // quitte le programme
+        printf("ERROR: Impossible to open %s (inexisting file ?).", argv[1]);
+        exit(-1); 
     }
 
-    int option;
+    readImgFormat(img); //lis le format du pbm
 
-    srand(time(NULL)); // initialisation de la generation aleatoire
+	readImgSize(img, &imgSize); //lis la taille du pbm.
 
-    if(argc == 1) // nombre d'aguments égale a 1
-    {
-        option = rand() % 3; // genere un nombre aleatoire
-    }
-    else
-    {
-        option = idArg1(argv[1]); // fonction qui est utilisé lorsqu'on rentre un 2eme argument lors de l'exécution du ScreenSaver
-    }
+    int imgPixel[imgSize.x][imgSize.y]; //créer un tableau de la taille du pbm.
 
-    switch(option)
-    {
-        case 0:
-            launchStatic(argc, argv[2]); //Execute le screenSaver statique en lui envoyant les bons paramètres.
-            break;
-        case 1:
-            launchDyna(argc, argv[2]); //Execute le screenSaver dynamique en lui envoyant les bons paramètres.
-            break;
-        case 2:
-            launchInter(argc, argv[2]); //Execute le screenSaver interactif en lui envoyant les bons paramètres.
-            break;
-        case 3:
-            readStats(); //Lit le fichier "history.txt" et l'affiche à l'écran.
-            break; 
-    }
+	readImgPixels(img, imgSize, imgPixel); //rempli le tableau avec les données binaires de l'image.
 
-    return 0;
+    fclose(img); //ferme le fichier image.
+
+    Frame imgFrame; //variable contenant un cadre
+
+    int continuee; //variable servant )à arrêter le programme.
+
+    continuee = 1;
+
+	pid = fork(); //création d'un processus fils
+
+	if(pid == -1)
+	{
+		printf("ERROR: Failure to create a new process."); //erreur en cas d'échec processus fils.
+		exit(-1);
+	}
+	else if(pid ==  0)
+	{
+		getch(); //attend que l'utilisateur tape sur son clavier.
+
+		kill(0, SIGTERM); //tue le processus dans la boucle infinie.
+	}
+	else
+	{
+		while(continuee)
+		{
+			switch(captureConsoleSize(&consoleSize, imgSize))
+			{
+				case -1:
+					continuee = 0; //arrête la boucle si l'image est plus petite que la console.
+					break;
+				case 1:
+					system("clear"); //nettoye le terminal visible.
+					
+					centerImg(imgSize, consoleSize, &imgFrame); //créer un cadre centrant l'image.
+					
+					printImg(consoleSize, imgSize, imgFrame, imgPixel); //affiche l'image sur la console.
+
+					break;
+			}
+
+			fflush(stdout);
+			sleep(1); //attend une seconde avant de répéter la boucle.
+		}
+	}
+
+	return 0;
 }
