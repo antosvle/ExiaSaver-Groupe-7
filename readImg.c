29301b9321d7efae5911222a5f322a0db6cb6d81
PierrotHml <pierre.hamel.eure@gmail.com>
2016-12-16 10:08:35 +0100

    MMU - permet de lire l'image termSaver3

--- readImg.c
+++ readImg.c
@@ -9,11 +9,11 @@ void readImgFormat(FILE *img)
 
 	positionCursor(img);
 
-	fgets(formatChain, 3, img);
+	fgets(formatChain, 3, img); //lecture du format du pbm.
 
 	formatChain[2] = '\0';
 
-	if(strcmp(formatChain, "P1") != 0)
+	if(strcmp(formatChain, "P1") != 0) //erreur format inconnu.
 	{
 		printf("ERROR: Unknown pbm format %s.", formatChain);
 		exit(-1);
@@ -28,7 +28,7 @@ void readImgSize(FILE *img, Coordinates *imgSize)
 
 	positionCursor(img);
 
-	fscanf(img, "%d", &imgSize->y);
+	fscanf(img, "%d", &imgSize->y); //récupérer les coordonnées pour les mettres dans imgSize.
 }
 
 void readImgPixels(FILE *img, Coordinates const imgSize, int imgPixel[imgSize.x][imgSize.y])
@@ -41,7 +41,29 @@ void readImgPixels(FILE *img, Coordinates const imgSize, int imgPixel[imgSize.x]
 		{
 			positionCursor(img);
 
-			fscanf(img, "%d", &imgPixel[i.x][i.y]);
+			fscanf(img, "%d", &imgPixel[i.x][i.y]); //mettre les valeurs binaires dans le tableau.
 		}
 	}
+}
+
+void positionCursor(FILE *img)
+{
+	int charRead;
+
+	charRead = fgetc(img);
+
+	while(charRead == '#' || charRead == '\n' || charRead == ' ') //tant que le caractère lu est # \n ou space, avancer d'un caractère.
+	{
+		if(charRead == '#')
+		{
+			while (charRead != '\n')
+			{
+				charRead = fgetc(img); //si un # est détecté, aller jusqu'à la prochaine ligne.
+			}
+		}
+
+		charRead = fgetc(img);
+	}
+
+	fseek(img, -1, SEEK_CUR); //reculer d'un caractère.
 }
