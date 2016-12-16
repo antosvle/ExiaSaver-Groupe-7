f3d326dfe480f69321dd081010d25f2073cc12f1
PierrotHml <pierre.hamel.eure@gmail.com>
2016-12-16 10:06:49 +0100

    MMU - fonctions diverses termSaver3

--- functions.c
+++ functions.c
@@ -1,55 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
 #include "functions.h"
 
-int captureConsoleSize(Coordinates *consoleSize, Coordinates const imgSize)
+void captureConsoleSize(Coordinates *consoleSize)
 {
 	struct winsize w;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-	if(consoleSize->x != w.ws_row - 1 || consoleSize->y != w.ws_col - 1);
-	{
-		consoleSize->x = w.ws_col - 1;
-		consoleSize->y = w.ws_row - 1; //capture de la taille de la console.
-
-		if(imgSize.x > consoleSize->x || imgSize.y > consoleSize->y)
-		{
-			printf("ERROR: The image is too large for this console size."); //si l'image est plus grande que la console, erreur
-			return -1;
-		}
-
-		return 1;
-	}
-
-	return 0;
+	
+	consoleSize->x = w.ws_col - 1;
+	consoleSize->y = w.ws_row - 1; //attribution de la taille de la console à consoleSize.
 }
 
-void positionCursor(FILE *img)
+void changeDirection(Direction *i)
 {
-	int charRead;
-
-	charRead = fgetc(img);
-
-	while(charRead == '#' || charRead == '\n' || charRead == ' ') //tant que le caractère est # \n ou space, lire le suivant
+	switch(getch()) //si getch prend z q s d, modification de i.
 	{
-		if(charRead == '#') //si caractère est #, aller à la ligne suivante
-		{
-			while (charRead != '\n')
-			{
-				charRead = fgetc(img);
-			}
-		}
-
-		charRead = fgetc(img);
+		case 'z':
+			*i = UP;
+			break;
+		case 'd':
+			*i = RIGHT;
+			break;
+		case 's':
+			*i = DOWN;
+			break;
+		case 'q':
+			*i = LEFT;
+			break;
 	}
-
-	fseek(img, -1, SEEK_CUR);
 }
 
-char getch() //fonction capturant un caractère dans le terminal.
+void moveImg(Direction const i, Coordinates const consoleSize, Coordinates *planePosition)
+{
+	switch(i)
+	{
+		case UP:
+			planePosition->y = planePosition->y - 1;
+			break;
+		case RIGHT:
+			planePosition->x = planePosition->x + 1;
+			break;
+		case DOWN:
+			planePosition->y = planePosition->y + 1;
+			break;
+		case LEFT:
+			planePosition->x = planePosition->x - 1; //modification des coordonnées de l'avion en fonction de i.
+			break;
+	}
+
+	if(planePosition->x > consoleSize.x)
+	{
+		planePosition->x = 0;
+	}
+	else if(planePosition->x < 0)
+	{
+		planePosition->x = consoleSize.x;
+	}
+
+	if(planePosition->y > consoleSize.y)
+	{
+		planePosition->y = 0;
+	}
+	else if (planePosition->y < 0)
+	{
+		planePosition->y = consoleSize.y; //si l'avion est hors du terminal visible, réapparait de l'autre côté.
+	}
+}
+
+char getch() //fonction retournant un caractère écrit dans le terminal.
 {
 	char buf = 0;
 	struct termios old = {0}; 
