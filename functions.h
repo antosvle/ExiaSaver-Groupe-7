9f279ee9c77ffbedbc2c1df43a7d48a6b71553a6
PierrotHml <pierre.hamel.eure@gmail.com>
2016-12-16 10:07:20 +0100

    MMU - prototypes des fonctions termSaver3

--- functions.h
+++ functions.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Frame Frame;
 struct Frame
 {
@@ -14,14 +17,23 @@ struct Coordinates
 	int y;
 };
 
+typedef enum Direction Direction;
+enum Direction
+{
+	UP, RIGHT, DOWN, LEFT
+};
+
 void readImgFormat(FILE *img);
 void readImgSize(FILE *img, Coordinates *imgSize);
 void readImgPixels(FILE *img, Coordinates const imgSize, int imgPixel[imgSize.x][imgSize.y]);
 
 void positionCursor(FILE *img);
 
-void centerImg(Coordinates const imgSize, Coordinates const consoleSize, Frame *imgFrame);
-void printImg(Coordinates const consoleSize, Coordinates const imgSize, Frame const imgFrame, int imgPixel[imgSize.x][imgSize.y]);
+void captureConsoleSize(Coordinates *consoleSize);
 
-int captureConsoleSize(Coordinates *consoleSize, Coordinates const imgSize);
+void moveImg(Direction const i, Coordinates const consoleSize, Coordinates *planePosition);
+void changeDirection(Direction *i);
 char getch();
+
+void frameImg(Coordinates const consoleSize, Coordinates const planePosition, Frame *imgFrame);
+void printImg(Coordinates const consoleSize, Frame const imgFrame, int imgPixel[6][6]);
